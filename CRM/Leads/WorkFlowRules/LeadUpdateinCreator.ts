70aa79de25ad9b8f01560efbf4c7efded180f3d0
meenatchisundaram7 <125951113+meenatchisundaram7@users.noreply.github.com>
2023-02-21 15:35:19 +0530

    Create LeadUpdateinCreator.ts

---
+++ CRM/Leads/WorkFlowRules/LeadUpdateinCreator.ts
@@ -0,0 +1,53 @@
+records = zoho.crm.getRecordById("Leads",LeadID);
+// info records ;
+CreateMap = Map();
+// CreateMap.put("CRM_ID",LeadID);
+CreateMap.put("First_Name",records.get("First_Name"));
+CreateMap.put("Last_Name",records.get("Last_Name"));
+CreateMap.put("Title",records.get("Designation"));
+CreateMap.put("Phone_Number",records.get("Phone"));
+CreateMap.put("Spouse_Name",records.get("Spouse_Name"));
+if(records.get("Date_of_Birth") != null && records.get("Date_of_Birth") != "")
+{
+	CreateMap.put("Date_of_Birth",records.get("Date_of_Birth").toDate());
+}
+CreateMap.put("Email",records.get("Email"));
+CreateMap.put("Skype_ID",records.get("Skype_ID"));
+CreateMap.put("Twitter",records.get("Twitter"));
+CreateMap.put("Email_Opt_Out",records.get("Email_Opt_Out"));
+CreateMap.put("Services",records.get("Service"));
+CreateMap.put("Company",records.get("Company"));
+CreateMap.put("No_of_Employees",records.get("No_of_Employees"));
+// CreateMap.put("Website", records.get("Website"));
+CreateMap.put("Industry",records.get("Industry"));
+CreateMap.put("Annual_Revenue",records.get("Average_Time_Spent_Minutes"));
+CreateMap.put("Lead_Source",records.get("Lead_Source"));
+CreateMap.put("Lead_Status",records.get("Lead_Status"));
+CreateMap.put("Quality_of_Lead",records.get("Quality_of_Leads"));
+CreateMap.put("Followupboss",records.get("Followupboss"));
+CreateMap.put("Old_Lead_Status",records.get("Old_Lead_Status"));
+CreateMap.put("Street",records.get("Street"));
+CreateMap.put("City",records.get("City"));
+CreateMap.put("State",records.get("State"));
+CreateMap.put("Zip_Code",records.get("Zip_Code"));
+CreateMap.put("Country",records.get("Country"));
+CreateMap.put("Description",records.get("Description"));
+if(records.get("First_Visited_Time") != null && records.get("First_Visited_Time") != "")
+{
+	CreateMap.put("First_Visit",records.get("First_Visited_Time").toDateTime());
+}
+CreateMap.put("Visitor_Score",records.get("Visitor_Score"));
+// CreateMap.put("Reference", records.get("Referrer"));
+CreateMap.put("Average",records.get("Average_Time_Spent_Minutes"));
+if(records.get("Last_Visited_Time") != null && records.get("Last_Visited_Time") != "")
+{
+	CreateMap.put("Most_Recent_Visit",records.get("Last_Visited_Time").toDateTime());
+}
+// CreateMap.put("First",records.get("First_Visited_URL"));
+CreateMap.put("Number_Of_Chats",records.get("Number_Of_Chats"));
+CreateMap.put("Days",records.get("Days_Visited"));
+CreateMap.put("From_CRM",true);
+info CreateMap;
+otherParams = Map();
+response = zoho.creator.updateRecord("inspectorempirebuilder","ieb","Leads_Report",records.get("Zoho_Creator_ID").toLong(),CreateMap,otherParams,"zohocreator");
+info response;
