ea799b5d764db6d1b5b16294011ab104ab0793fa
meenatchisundaram7 <125951113+meenatchisundaram7@users.noreply.github.com>
2023-02-21 17:28:10 +0530

    Create DeleteMemberInCreator.ts

---
+++ CRM/Members/WorkflowRules/DeleteMemberInCreator.ts
@@ -0,0 +1,11 @@
+if(recID != null)
+{
+	recordid = recID.toLong();
+	deleteRecord = invokeurl
+	[
+		url :"https://creator.zoho.com/api/v2/inspectorempirebuilder/ieb/report/All_Members/" + recordid
+		type :DELETE
+		connection:"zohocreator"
+	];
+}
+info deleteRecord;
