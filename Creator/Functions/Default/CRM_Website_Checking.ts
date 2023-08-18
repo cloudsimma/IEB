8b7694990dd5e433c26ff783fadc4daaf233591c
NambiRajanCL <125959465+NambiRajanCL@users.noreply.github.com>
2023-08-18 14:36:50 +0530

    Create CRM_Website_Checking.ts

---
+++ Creator/Functions/Default/CRM_Website_Checking.ts
@@ -0,0 +1,6 @@
+void CRM_Website_Checking(string URL)
+{
+	regex_URL = "^(http:\/\/www.|https:\/\/www.|ftp:\/\/www.|www.|http:\/\/|https:\/\/|ftp:\/\/|){1}[^\x00-\x19\x22-\x27\x2A-\x2C\x2E-\x2F\x3A-\x40\x5B-\x5E\x60\x7B\x7D-\x7F]+(\.[^\x00-\x19\x22\x24-\x2C\x2E-\x2F\x3C\x3E\x40\x5B-\x5E\x60\x7B\x7D-\x7F]+)+(\/[^\x00-\x19\x22\x3C\x3E\x5E\x7B\x7D-\x7D\x7F]*)*$";
+	url_check = URL.matches(regex_URL);
+	info url_check;
+}
