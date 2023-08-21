10a9e52616a87a512826869b6872020f3a371f8f
NambiRajanCL <125959465+NambiRajanCL@users.noreply.github.com>
2023-08-21 15:06:04 +0530

    Create DisablingOnboardingFields.ts
    disabling fields on Create and Edit - onload

---
+++ CRM/Client Scripts/Members/DisablingOnboardingFields.ts
@@ -0,0 +1,8 @@
+var field_obj_01 = ZDK.Page.getField('Onboarding_Stages');
+field_obj_01.setReadOnly(true);
+var field_obj_02 = ZDK.Page.getField('Current_Onboarding_Stage');
+field_obj_02.setReadOnly(true);
+var field_obj_03 = ZDK.Page.getField('Remainder');
+field_obj_03.setReadOnly(true);
+var field_obj_04 = ZDK.Page.getField('Workflow_Date');
+field_obj_04.setReadOnly(true);
